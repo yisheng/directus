ab48a37be1ed998f1ad8eaefbc4c2eaa9a8a42c4
yisheng <yang@desheng.me>
2022-10-30 23:38:53 +0800

    更新「姓」、「名」的展示模板

--- app/src/utils/user-name.ts
+++ app/src/utils/user-name.ts
@@ -7,11 +7,15 @@ export function userName(user?: Partial<User>): string {
 	}
 
 	if (user.first_name && user.last_name) {
-		return `${user.first_name} ${user.last_name}`;
+		return `${user.last_name}${user.first_name}`;
 	}
 
 	if (user.first_name) {
 		return user.first_name;
+	}
+
+	if (user.last_name) {
+		return user.last_name;
 	}
 
 	if (user.email) {
