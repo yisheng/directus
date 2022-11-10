a2de42a5cdf5b5ef015ffbd33f94a469e4f59ef7
yisheng <yang@desheng.me>
2022-11-10 14:49:11 +0800

    New tools: updateVersions

---
+++ utils.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+// Usage: node ./utils.js 9.21.1
+
+const command = process.argv[2];
+const params = process.argv[3];
+switch (command) {
+    case 'updateVersions': {
+        updateVersions(params);
+        break;
+    }
+    default: {
+        console.error('Invalid command.');
+    }
+}
+
+function updateVersions(newVersion) {
+    const folders = [
+        './api/',
+        './app/',
+        './packages/create-directus-extension/',
+        './packages/drive/',
+        './packages/drive-azure/',
+        './packages/drive-gcs/',
+        './packages/drive-s3/',
+        './packages/extensions-sdk/',
+        './packages/schema/',
+        './packages/shared/',
+        './packages/specs/'
+    ];
+    folders.forEach((folder) => {
+        var filePath = folder + 'package.json';
+        var content = require(filePath);
+        content.version = newVersion;
+
+        fs.writeFile(filePath, JSON.stringify(content, null, '	') + '\n', function writeJSON(err) {
+            if (err) {
+                console.log(filePath + ' - error: ' + err);
+            }else {
+                console.log(filePath + ' - done');
+            }
+        });
+    })
+}
